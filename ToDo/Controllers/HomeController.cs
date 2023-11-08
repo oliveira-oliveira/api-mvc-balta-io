1f7b938faeedbd04ee583fbb0a1eb5f484a7dd10
Diego Oliveira <diego.oliveira@grupocheck.com.br>
2023-11-08 16:17:44 -0300

    criação do PUT e DELETE

--- ToDo/Controllers/HomeController.cs
+++ ToDo/Controllers/HomeController.cs
@@ -17,14 +17,53 @@ namespace ToDo.Controllers
             return context.ToDos.ToList();
         }
         
+        [HttpGet("/{id:int}")]
+        public ToDoModel GetById([FromRoute] int id,
+                                 [FromServices] AppDbContext context)
+        {
+
+            return context.ToDos.FirstOrDefault(x => x.Id == id);
+        }
+        
         [HttpPost("/")]
         public ToDoModel Post([FromBody] ToDoModel todo, 
-                            [FromServices] AppDbContext context)
+                              [FromServices] AppDbContext context)
         {
             context.ToDos.Add(todo);
             context.SaveChanges();
 
             return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public ToDoModel Put([FromRoute] int id,
+                             [FromBody] ToDoModel toDo, 
+                             [FromServices] AppDbContext context)
+        {
+            var model = context.ToDos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return null;
+            
+            model.Title = toDo.Title;
+            model.Done = toDo.Done;
+
+            context.ToDos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        
+        [HttpDelete("/{id:int}")]
+        public ToDoModel Delete([FromRoute] int id,
+                             [FromServices] AppDbContext context)
+        {
+            var model = context.ToDos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return null;
+
+            context.ToDos.Remove(model);
+            context.SaveChanges();
+            return model;
         }
 
     }
